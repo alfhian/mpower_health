dbc3a4c12494e66adfac08debc55fe7807344352
alfhiansuwandi <alfhian@drrewire.com>
2023-03-30 10:15:13 +0700

    2fa via email removed and add vendor folder

--- symlink.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $target = $_SERVER['DOCUMENT_ROOT']."/../laravel/storage/app/publicsymlink.php";
-    $link = $_SERVER['DOCUMENT_ROOT']."/storage";
-    if(symlink( $target, $link )){
-        echo "OK.";
-    } else {
-        echo "Gagal.";
-    }
-?>
